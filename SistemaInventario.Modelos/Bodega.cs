b9eb339b599d0b2c1305d341de7a958a73584ea5
Sebastian Naranjo <sebas2013n@gmail.com>
2023-12-11 15:44:25 -0500

    Base de datos y primer modelo

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es requerido")]
+        [MaxLength(60, ErrorMessage ="El nombre no debe superar los 60 Caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [MaxLength(100, ErrorMessage = "La descripción no debe superar los 100 Caracteres")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
