6024712b43c3b7579d590085f314f4a29e08e038
Sebastian Naranjo <sebas2013n@gmail.com>
2024-01-26 22:00:12 -0500

    Inicio CRUD

---
+++ SistemaInventario/Areas/Admin/Controllers/BodegaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IReporsitorio;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public BodegaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Bodega.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        #endregion
+
+    }
+}
