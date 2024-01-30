8adebb98c94c8c8f7077e2541a4d9ad6d730b5bf
Sebastian Naranjo <sebas2013n@gmail.com>
2024-01-29 22:47:14 -0500

    Upsert

--- SistemaInventario/Areas/Admin/Controllers/BodegaController.cs
+++ SistemaInventario/Areas/Admin/Controllers/BodegaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SistemaInventario.AccesoDatos.Repositorio.IReporsitorio;
+using SistemaInventario.Modelos;
 
 namespace SistemaInventario.Areas.Admin.Controllers
 {
@@ -16,6 +17,25 @@ namespace SistemaInventario.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Bodega bodega = new Bodega();
+
+            if(id == null)
+            {
+                bodega.Estado = true;
+                return View(bodega);
+            }
+
+            bodega = await _unidadTrabajo.Bodega.Obtener(id.GetValueOrDefault());
+            if(bodega == null)
+            {
+                return NotFound();
+            }
+
+            return View(bodega);
         }
 
         #region API
